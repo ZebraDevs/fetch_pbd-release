dc95ac94b971905beff737854a1282f0b9b5c1ae
Sarah Elliott <sksellio@gmail.com>
2017-04-20 19:13:41 -0700

    Revert "Trimmed the branch to only the fetch_arm_control sub directory"
    This reverts commit 4895ae99c266c24474cc566a889565c3a19662fc.

---
+++ fetch_pbd_interaction/web_interface/fetch-pbd-gui/install_web_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm use --delete-prefix v7.0.0 --silent
+nvm install node
+npm install bower
+npm install polymer-cli
+./node_modules/bower/bin/bower install
