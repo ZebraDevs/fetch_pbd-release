394296af52b945d853f9ff85270cba5205c04cfd
Sarah Elliott <sksellio@gmail.com>
2017-08-08 13:52:33 -0700

    Trimmed the branch to only the fetch_social_gaze sub directory

--- fetch_pbd_interaction/scripts/serve_web_interface.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-path=`rospack find fetch_pbd_interaction`
-cd $path/web_interface/fetch-pbd-gui/compiled_frontend/bundled-es6
-python -m SimpleHTTPServer 8080
--- fetch_pbd_interaction/web_interface/fetch-pbd-gui/build_frontend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-./node_modules/polymer-cli/bin/polymer.js build
-rm -r compiled_frontend
-mv build compiled_frontend
